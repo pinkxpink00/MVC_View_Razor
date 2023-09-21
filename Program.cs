2b73abca12fc05623bd854645ce54cc50e9f1abb
pinkxpink00 <kyperu07@gmail.com>
2023-09-21 23:11:16 +0300

    delete Products

--- Program.cs
+++ Program.cs
@@ -5,7 +5,10 @@ builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
 app.MapControllerRoute(
-	name: "default",
-	pattern: "{controller=Home}/{action=Index}/{id?}");
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.MapControllerRoute(
+    name: "product",
+    pattern: "{controller=Products}/{action=Product}/{id?}");
 app.Run();
