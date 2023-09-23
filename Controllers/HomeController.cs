94640fa1429b6b5eb1a6423117c268c140fd66aa
pinkxpink00 <kyperu07@gmail.com>
2023-09-23 10:55:14 +0300

    used prefix

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,9 +2,12 @@
 
 namespace MvcApp.Controllers
 {
+	[Route("Home")]
 	public class HomeController : Controller
 	{
+		[Route("Index")]
 		public string Index(int id) => $"Index Page. Id:{id}";
+		[Route("About")]
 		public string About(string name, int age)=>$"About page. Name:{name}, Age:{age}";
 	}
 }
--- Program.cs
+++ Program.cs
@@ -4,10 +4,5 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller}/{action}/{id}");
-app.MapControllerRoute(
-    name:"name_age",
-    pattern:"{controller}/{action}/{name}/{age}");
+app.MapControllers();
 app.Run();
