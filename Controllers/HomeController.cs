776b8575c6b1c56877211d9e1f4afd3c02aa517f
pinkxpink00 <kyperu07@gmail.com>
2023-09-23 12:30:42 +0300

    used models

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,13 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MvcApp.Models;
 
 namespace MvcApp.Controllers
 {
-	[Route("Home")]
+
 	public class HomeController : Controller
 	{
-		[Route("Index")]
-		public string Index(int id) => $"Index Page. Id:{id}";
-		[Route("About")]
-		public string About(string name, int age)=>$"About page. Name:{name}, Age:{age}";
+		List<Person> people = new List<Person>
+		{
+			new Person (1,"German", 24),
+			new Person (2,"Jop",44),
+			new Person (3,"Mooff",45)
+		};
+		[Route("Home/Index")]
+		public IActionResult Index()
+		{
+			return View(people);
+		}
 	}
 }
--- Models/Person.cs
+++ Models/Person.cs
@@ -2,5 +2,17 @@
 {
     public class Person
     {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public int Age { get; set; }
+
+        public Person(int id, string name, int age)
+        {
+            Id = id;
+            Name = name;
+            Age= age;
+        }
+
+        public string PrintInfo() => $"{Id}: {Name} - ({Age})";
     }
 }
